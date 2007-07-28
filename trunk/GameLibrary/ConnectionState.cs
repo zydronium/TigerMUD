b7f086051b6c0363497c5af1f1ca085dbdfa421a
SND\adammil_cp <SND\adammil_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2007-07-28 15:37:33 +0000

    git-svn-id: https://tigermud.svn.codeplex.com/svn@8690 142e9c35-602f-4880-9f37-ec48a24daa6a

--- trunk/GameLibrary/ConnectionState.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GameLibrary
-{
-    public enum ConnectionState { NewConnection = 0, WaitingforClientResponse, NotWaitingForClientResponse, NewUser, WaitingAtMenuPrompt, QuestionPrompt, QuestionResponse, CommandPrompt, CommandResponse, Disconnected, ConnectedNotWaitingForClient };
-}
--- trunk/GameLibrary/MenuType.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GameLibrary
-{
-    public enum MenuType { SingleChoice = 0, MultiChoice, Question };
-}
