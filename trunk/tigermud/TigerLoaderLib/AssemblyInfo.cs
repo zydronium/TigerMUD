0583fa3e7220fbd94217c0bdcd854c0c62506c74
SND\adammil_cp <SND\adammil_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2008-03-15 20:31:41 +0000

    Removed tigerloaderlib signing and the setup project so SharpDevelop 2.2 will work on this code
    git-svn-id: https://tigermud.svn.codeplex.com/svn@16796 142e9c35-602f-4880-9f37-ec48a24daa6a

--- trunk/tigermud/TigerLoaderLib/AssemblyInfo.cs
+++ trunk/tigermud/TigerLoaderLib/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.CompilerServices;
 //       documentation for more information on this.
 //
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("strongnamekey.snk")]
+[assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
